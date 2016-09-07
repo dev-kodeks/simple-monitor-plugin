ba56924a8ef104831fc6bbeb7dfe6e5e171d6a4e
alex <murzilka17@gmail.com>
2016-09-07 19:31:18 +0300

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+const inspect = require('util').inspect,
+	http = require('http'),
+	url = require('url');
+
+console.log('test plugin, index.js file executed');
+
+const server = http.createServer((req, res) => {
+	console.log(`plugin ${global.KodeksApi.Name} , request`);
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end(`request: ${url.format(req.url)}`
+		+ `, responce: okay from plugin ${global.KodeksApi.Name}`);
+});
+server.on('error', (e) => {
+	console.error(`plugin ${global.KodeksApi.Name} error: ${inspect(e)}`);
+});
+server.listen(global.KodeksApi.SocketPath, function () {
+	console.log(`plugin ${global.KodeksApi.Name} opened server on ${server.address()}`);
+});
