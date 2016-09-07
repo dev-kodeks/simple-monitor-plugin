5347a606cc9241f55dee0e2af3b08ad0799a10a3
alex <murzilka17@gmail.com>
2016-09-07 21:05:27 +0300

    enable strict mode

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const inspect = require('util').inspect,
-	http = require('http'),
-	url = require('url');
+'use strict';
+
+const inspect = require('util').inspect
+	, http = require('http')
+	, url = require('url')
+	;
 
 console.log('test plugin, index.js file executed');
 
 const server = http.createServer((req, res) => {
 	console.log(`plugin ${global.KodeksApi.Name} , request`);
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end(`request: ${url.format(req.url)}`
-		+ `, responce: okay from plugin ${global.KodeksApi.Name}`);
+	res.end(`request: ${url.format(req.url)}
+		, responce: okay from plugin ${global.KodeksApi.Name}`);
 });
 server.on('error', (e) => {
 	console.error(`plugin ${global.KodeksApi.Name} error: ${inspect(e)}`);
