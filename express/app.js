a4877d2bcf2cfc0306078ecc6e05d91762961129
alex <murzilka17@gmail.com>
2017-01-09 15:17:10 +0300

    Добавил извлечение userId из куки, пришедшей от пользователя с доменной авторизацией. Добавил форматирование выводов тестовых плагинов.

--- express/app.js
+++ express/app.js
@@ -2,7 +2,6 @@
 
 const http = require('http')
 	, url = require('url')
-	, inspect = require('util').inspect
 	, express = require('express')
 	, app = express()
 	, cookieParser = require('cookie-parser')
@@ -11,28 +10,55 @@ const http = require('http')
 app.use(cookieParser());
 
 app.use(function (req, res, next) {
-	if (!req.cookies || !req.cookies.Auth) return next();
-	req.userId = new Buffer(req.cookies.Auth, 'base64').toString('ascii').split(':')[0];
+	if (!req.cookies) return next();
+	
+	let userId = null;
+	if (req.cookies.ClientUser) {
+		req.userId = req.cookies.ClientUser;
+	} else if (req.cookies.Auth) {
+		req.userId = new Buffer(req.cookies.Auth, 'base64').toString('ascii').split(':')[0];
+	}
+	
+	if (!req.userId) return next();
+	
 	global.KodeksApi.UserInfo(req.userId)
-		.then(userInfo => {
-			req.userInfo = userInfo;
-			return next();
-		})
-		.catch(error => {
-			req.userInfo = { error: error };
-			return next();
-		});
+	.then(userInfo => {
+		try {
+			req.userInfo = JSON.parse(userInfo);
+		} catch (err) {
+			req.userInfo = { error: err.toString() };
+		}
+		return next();
+	})
+	.catch(error => {
+		try {
+			req.userInfo = JSON.parse(error);
+		} catch (err) {
+			req.userInfo = { error: error.toString() };
+		}
+		return next();
+	});
 });
+
+const kPadding = 4;
 
 app.all('*', function (req, res) {
 	console.log(`plugin ${global.KodeksApi.Name} , request`);
-
-	res.send(`plugin ${global.KodeksApi.Name}<br>
-		userId: ${req.userId}<br>
-		userInfo: ${inspect(req.userInfo)}<br>
-		request: ${req.method} ${url.format(req.url)}<br>
-		request.headers: ${inspect(req.headers)}<br>
-		responce: okay from plugin ${global.KodeksApi.Name}`);
+	res.type('html');
+	res.status(200);
+	let str = '<html><body>';
+	str += `<h1>Plugin ${global.KodeksApi.Name}</h1>`;
+	str += `<h2>${req.method} ${url.format(req.url)}</h2>`;
+	str += '<hr>';
+	str += `<h3>User info:</h3>
+		<pre>${req.userInfo ? JSON.stringify(req.userInfo, null, kPadding) : 'no user id'}</pre>`;
+	str += '<hr>';
+	str += `<h3>Request headers:</h3><pre>${JSON.stringify(req.headers, null, kPadding)}</pre>`;
+	str += '<hr>';
+	str += `<h3>KodeksApi:</h3><pre>${JSON.stringify(KodeksApi
+		, (k, v) => (typeof v === 'function') ? 'FUNCTION' : v, kPadding)}</pre>`;
+	str += '</body></html>';
+	res.send(str);
 });
 
 app.listen(global.KodeksApi.SocketPath, function () {
--- simple/index.js
+++ simple/index.js
@@ -1,23 +1,71 @@
 'use strict';
 
-const inspect = require('util').inspect
-	, http = require('http')
+const http = require('http')
 	, url = require('url')
+	, querystring = require(('querystring'))
 	;
 
-console.log('test plugin, index.js file executed');
+const kPadding = 4;
+
+function finalHandler(req, res) {
+	res.writeHead(200, { 'Content-Type': 'Content-Type: text/html; charset=utf-8' });
+	res.write('<html><body>');
+	res.write(`<h1>Plugin ${global.KodeksApi.Name}</h1>`);
+	res.write(`<h2>Request: ${req.method} ${url.format(req.url)}</h2>`);
+	res.write('<hr>');
+	res.write(`<h3>Request body: ${req.body}</h3>`);
+	res.write('<hr>');
+	res.write(`<h3>User info:</h3>
+		<pre>${req.userInfo ? JSON.stringify(req.userInfo, null, kPadding) : 'no user id'}</pre>`);
+	res.write('<hr>');
+	res.write(`<h3>Request headers:</h3><pre>${JSON.stringify(req.headers, null, kPadding)}</pre>`);
+	res.write('<hr>');
+	res.write(`<h3>KodeksApi:</h3><pre>${JSON.stringify(KodeksApi
+		, (k, v) => (typeof v === 'function') ? 'FUNCTION' : v, kPadding)}</pre>`);
+	res.write('</body></html>');
+	res.end();
+}
 
 const server = http.createServer((req, res) => {
 	console.log(`plugin ${global.KodeksApi.Name} , request`);
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end(`plugin ${global.KodeksApi.Name}
-		request: ${url.format(req.url)}
-		request.headers: ${inspect(req.headers)}
-		responce: okay from plugin ${global.KodeksApi.Name}`);
+
+	let body = [];
+	req.on('data', chunk => body.push(chunk))
+	.on('end', () => {
+		if (body) req.body = Buffer.concat(body).toString();
+
+		if (!req.headers.cookie) return finalHandler(req, res);
+	
+		const cookie = querystring.parse(req.headers.cookie, '; ');
+		let userId = null;
+		if (cookie.ClientUser) userId = cookie.ClientUser;
+		else if (cookie.Auth) userId = new Buffer(cookie.Auth, 'base64').toString('ascii').split(':')[0];
+		
+		if (!userId) return finalHandler(req, res);
+
+		req.userId = userId;
+		global.KodeksApi.UserInfo(req.userId)
+		.then(userInfo => {
+			try {
+				req.userInfo = JSON.parse(userInfo);
+			} catch (err) {
+				req.userInfo = { error: err.toString() };
+			}
+			return finalHandler(req, res);
+		})
+		.catch(error => {
+			try {
+				req.userInfo = JSON.parse(error);
+			} catch (err) {
+				req.userInfo = error.toString();
+			}
+			return finalHandler(req, res);
+		});
+	});
+}).on('error', (e) => {
+	console.error(`plugin ${global.KodeksApi.Name} error: ${e.toString()}`);
 });
-server.on('error', (e) => {
-	console.error(`plugin ${global.KodeksApi.Name} error: ${inspect(e)}`);
-});
+
 server.listen(global.KodeksApi.SocketPath, function () {
 	console.log(`plugin ${global.KodeksApi.Name} opened server on ${server.address()}`);
 });
