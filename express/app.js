ca1711591557ce0e853ec1c61322845ad94cd477
garry <garry@kodeks.ru>
2017-01-18 22:39:17 +0300

    Обновлен пример с express'ом
    Summary:
     - `KodeksApi` заменен на `KServerApi`
     - добавлены api-методы `CheckAccess` и `KodeksDocInfo`

--- express/app.js
+++ express/app.js
@@ -4,24 +4,11 @@ const http = require('http')
 	, url = require('url')
 	, express = require('express')
 	, app = express()
-	, cookieParser = require('cookie-parser')
 	;
 
-app.use(cookieParser());
-
+// UserInfo
 app.use(function (req, res, next) {
-	if (!req.cookies) return next();
-
-	let userId = null;
-	if (req.cookies.ClientUser) {
-		req.userId = req.cookies.ClientUser;
-	} else if (req.cookies.Auth) {
-		req.userId = new Buffer(req.cookies.Auth, 'base64').toString('ascii').split(':')[0];
-	}
-	
-	if (!req.userId) return next();
-
-	global.KodeksApi.UserInfo(req.userId)
+	global.KServerApi.UserInfo(req)
 	.then(userInfo => {
 		try {
 			req.userInfo = JSON.parse(userInfo);
@@ -40,27 +27,76 @@ app.use(function (req, res, next) {
 	});
 });
 
+// CheckAccess
+app.use(function (req, res, next) {
+	global.KServerApi.CheckAccess(4360, req)
+	.then(access => {
+		try {
+			req.checkAccess = JSON.parse(access);
+		} catch (err) {
+			req.checkAccess = { error: err.toString() };
+		}
+		return next();
+	})
+	.catch(error => {
+		try {
+			req.checkAccess = JSON.parse(error);
+		} catch (err) {
+			req.checkAccess = { error: error.toString() };
+		}
+		return next();
+	});
+});
+
+// KodeksDocInfo
+app.use(function (req, res, next) {
+	global.KServerApi.KodeksDocInfo(9027690, req)
+	.then(docInfo => {
+		try {
+			req.kodeksDocInfo = JSON.parse(docInfo);
+		} catch (err) {
+			req.kodeksDocInfo = { error: err.toString() };
+		}
+		return next();
+	})
+	.catch(error => {
+		try {
+			req.kodeksDocInfo = JSON.parse(error);
+		} catch (err) {
+			req.kodeksDocInfo = { error: error.toString() };
+		}
+		return next();
+	});
+});
+
+
 const kPadding = 4;
 
 app.all('*', function (req, res) {
-	console.log(`plugin ${global.KodeksApi.Name} , request`);
+	console.log(`plugin ${global.KServerApi.Name} , request`);
 	res.type('html');
 	res.status(200);
 	let str = '<html><body>';
-	str += `<h1>Plugin ${global.KodeksApi.Name}</h1>`;
+	str += `<h1>Plugin ${global.KServerApi.Name}</h1>`;
 	str += `<h2>${req.method} ${url.format(req.url)}</h2>`;
 	str += '<hr>';
 	str += `<h3>User info:</h3>
-		<pre>${req.userInfo ? JSON.stringify(req.userInfo, null, kPadding) : 'no user id'}</pre>`;
+		<pre>${req.userInfo ? JSON.stringify(req.userInfo, null, kPadding) : 'some problem'}</pre>`;
+	str += '<hr>';
+	str += `<h3>Check access:</h3>
+		<pre>${req.checkAccess ? JSON.stringify(req.checkAccess, null, kPadding) : 'some problem'}</pre>`;
+	str += '<hr>';
+	str += `<h3>Kodeks doc info:</h3>
+		<pre>${req.kodeksDocInfo ? JSON.stringify(req.kodeksDocInfo, null, kPadding) : 'some problem'}</pre>`;
 	str += '<hr>';
 	str += `<h3>Request headers:</h3><pre>${JSON.stringify(req.headers, null, kPadding)}</pre>`;
 	str += '<hr>';
-	str += `<h3>KodeksApi:</h3><pre>${JSON.stringify(KodeksApi
+	str += `<h3>KServerApi:</h3><pre>${JSON.stringify(KServerApi
 		, (k, v) => (typeof v === 'function') ? 'FUNCTION' : v, kPadding)}</pre>`;
 	str += '</body></html>';
 	res.send(str);
 });
 
-app.listen(global.KodeksApi.SocketPath, function () {
-	console.log(`Example app listening on ${global.KodeksApi.SocketPath}!`);
+app.listen(global.KServerApi.SocketPath, function () {
+	console.log(`Example app listening on ${global.KServerApi.SocketPath}!`);
 });
